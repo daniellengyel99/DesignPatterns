474e198c226323cb3c27517caf690f098c7dd7ae
Daniel Lengyel <daniel.lengyel99@gmail.com>
2020-10-27 11:49:50 +0200

    Lab5 Commit

--- src/Image.java
+++ src/Image.java
@@ -1,13 +1,20 @@
+import java.util.concurrent.TimeUnit;
+
 public class Image implements Element{
 
-    private String name;
+    private String imageName;
 
     Image(String name){
-        this.name=name;
+        this.imageName=name;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void print(){
-        System.out.println("Image with name: "+this.name);
+        System.out.println("Image with name: "+this.imageName);
     }
 
 }
