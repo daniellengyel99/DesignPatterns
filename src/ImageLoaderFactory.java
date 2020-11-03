fd21347f824d4f3c46eefad477e01ed2c696667e
Daniel Lengyel <daniel.lengyel99@gmail.com>
2020-11-03 12:47:00 +0200

    Lab6 Second Part Commit

---
+++ src/ImageLoaderFactory.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class ImageLoaderFactory {
+
+    public static void create(String url) throws IOException {
+        if (url.toLowerCase().endsWith("bmp")) {
+            BMPImageLoader.load(url);
+        }
+        else if (url.toLowerCase().endsWith("jpg")) {
+            JPGImageLoader.load(url);
+        }
+        else if (url.toLowerCase().endsWith("png")) {
+            PNGImageLoader.load(url);
+        }
+        throw new RuntimeException("Unsupported format: " + url);
+    }
+
+}
