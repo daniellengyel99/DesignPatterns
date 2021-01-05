d29d5753f5657eb6814c75e882faa4cca18f9339
Daniel Lengyel <daniel.lengyel99@e-uvt.ro>
2021-01-05 12:42:00 +0200

    Lab11 final

--- src/main/java/rouvt/models/FirstObserver.java
+++ src/main/java/rouvt/models/FirstObserver.java
@@ -3,6 +3,6 @@ package rouvt.models;
 public class FirstObserver implements Observer{
     @Override
     public void update(String oldValue, String newValue) {
-        System.out.println("old value: "+oldValue+" and new value: "+newValue);
+        System.out.println("FIRST OBSERVER:= old value: "+oldValue+" and new value: "+newValue);
     }
 }
--- src/main/java/rouvt/models/SecondObserver.java
+++ src/main/java/rouvt/models/SecondObserver.java
@@ -3,6 +3,6 @@ package rouvt.models;
 public class SecondObserver implements Observer{
     @Override
     public void update(String oldValue, String newValue) {
-        System.out.println("old value: "+oldValue+" and new value: "+newValue);
+        System.out.println("SECOND OBSERVER:= old value: "+oldValue+" and new value: "+newValue);
     }
 }
